f8f418533f8452c676e350e2e8868c17d459209e
Praveen <prrraveen@gmail.com>
2016-02-26 10:38:41 +0530

    * removed unwanted gem jekyll-assets. * Cleaned index.html. Moved carousel related data(images , tagline etc) from index.html to _date/slider_images.json * Perhaps there should not be font-matter in index.html. Font matter is only applied to markdown pages. * cleaned code for carouse.html * cleaned main.js

--- js/main.js
+++ js/main.js
@@ -1,106 +1,40 @@
-jQuery(function($) {'use strict',
+/**
+ * @init is function run after loading the page. It is the first function executed.
+ *
+ * */
 
-	//#main-slider
-	$(function(){
-		$('#main-slider .carousel').carousel({
-			interval: 6000,
-			pause: "false"
-		});
-	});
+var init;
+init = function () {
+    //The .active class needs to be added to one of the slides. Otherwise, the carousel will not be visible.
+    $('.carousel .item:first').addClass('active');
 
-	// Search
-	$('.fa-search').on('click', function() {
-		$('.field-toggle').fadeToggle(300);
-	});
+    //adding carousel avoid using bootstrap data tags for JS events. it get un manageable soon.
+    $('.carousel').carousel({
+        interval: 2000
+    });
 
-
-	// accordian
-	$('.accordion-toggle').on('click', function(){
-		$(this).closest('.panel-group').children().each(function(){
-		$(this).find('>.panel-heading').removeClass('active');
-		 });
-
-	 	$(this).closest('.panel-heading').toggleClass('active');
-	});
-
-	//Initiat WOW JS
-	new WOW().init();
-
-	// portfolio filter
-	$(window).load(function(){'use strict';
-		var $portfolio_selectors = $('.portfolio-filter >li>a');
-		var $portfolio = $('.portfolio-items');
-		$portfolio.isotope({
-			itemSelector : '.portfolio-item',
-			layoutMode : 'fitRows'
-		});
-		
-		$portfolio_selectors.on('click', function(){
-			$portfolio_selectors.removeClass('active');
-			$(this).addClass('active');
-			var selector = $(this).attr('data-filter');
-			$portfolio.isotope({ filter: selector });
-			return false;
-		});
-	});
-	
-	if ( ($(window).height() + 100) < $(document).height() ) {
-	    $('#top-link-block').removeClass('hidden').affix({
-	        // how far to scroll down before link "slides" into view
-	        offset: {top:100}
-	    });
-	}
-	//goto top
-	$('.gototop').click(function(event) {
-		event.preventDefault();
-		$('html, body').animate({
-			scrollTop: $("body").offset().top
-		}, 500);
-	});	
+    //apply transparent navbar
+    apply_on_scroll();
+};
 
 
-	//Pretty Photo
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
-	});
-	$('.lazy').slick({
-		  lazyLoad: 'ondemand',
-		  slidesToShow: 3,
-		  slidesToScroll: 1,
-		  autoplay: true,
-		  autoplaySpeed: 2000,
-		   responsive: [
-		    {
-		      breakpoint: 1024,
-		      settings: {
-		        slidesToShow: 2,
-		        slidesToScroll: 1,
-		        infinite: true,
-		        dots: true
-		      }
-		    },
-		    {
-		      breakpoint: 600,
-		      settings: {
-		        slidesToShow: 1,
-		        slidesToScroll: 1
-		      }
-		    },
-		    {
-		      breakpoint: 480,
-		      settings: {
-		        slidesToShow: 1,
-		        slidesToScroll: 1
-		      }
-		    }
-		    // You can unslick at a given breakpoint now by adding:
-		    // settings: "unslick"
-		    // instead of a settings object
-		  ]
-		});
-	//load more posts in blog page
-	$('#loadmore').on('click', function(){
-		$('#moreposts').css('display', 'block');
-		$(this).css('display', 'none');
-	});
-});
+var apply_on_scroll = function(){
+    /**
+     *
+     * initially the nav bar is transparent to keep the image uncluttered. but if user scrolls down. A flat ui nav bar is displayed.
+     *
+     */
+    var _this = $('.navigation');
+    _this.addClass('transparent');
+    $(window).scroll(function (event) {
+        var scroll = $(window).scrollTop();
+        if(scroll > 150){
+            _this.removeClass('transparent');
+        }
+        else {
+            _this.addClass('transparent');
+        }
+    });
+};
+
+window.onload = init(); //when dom is loaded. run init function
